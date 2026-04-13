3a2f323176c0cbeb38421a505814b38ed8ae9c0e
zestymec <m.umer394aziz@gmail.com>
2026-04-13 16:24:15 +0500

    craetes password logic created

--- App.tsx
+++ App.tsx
@@ -1,19 +1,48 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import * as Yup from 'yup'
 const passwordSchema = Yup.object().shape({
   passwordLength: Yup.number().min(2, 'should be min 2').max(8, 'should be less than 8').required('length is required')
 })
 
 export default function App() {
+  const [password, setPassword] = useState('')
+  const [ispassgenerated, setIspassgenerated] = useState(false)
+  const [lowercase, setlowercase] = useState(true)
+  const [Uppercase, setUpercase] = useState(false)
+  const [numbers, setNumbers] = useState(false)
+  const [symbols, setIsymbols] = useState(false)
+
+  const generatepasswordString = (passwordlength: number) => {
+    //
+  }
+  const createPasswords = (chracters: string, passwordlength: number) => {
+    let result = ''
+    for (let i = 0; i < passwordlength; i++) {
+      const chracterIndex = Math.round(Math.random() * chracters.length)
+      result += chracters.charAt(chracterIndex)
+    } return result
+  }
+  const resetPasswords = () => {
+    //
+  }
 
   return (
-    <View>
-      <Text>App lore
 
+    <View>
+      <Text style={styles.heading}>
+
+        Lahore Lahore ay !
       </Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  heading: {
+    margin: 'auto',
+    fontWeight: 'bold',
+    fontSize: 45,
+    paddingVertical: 22,
+  }
+})
