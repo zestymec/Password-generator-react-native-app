6dd12b3b0f39da443962917db0b0035a4250e623
zestymec <m.umer394aziz@gmail.com>
2026-04-13 12:23:45 +0500

    you package installed and set uped

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import * as Yup from 'yup'
+const passwordSchema = Yup.object().shape({
+  passwordLength: Yup.number().min(2, 'should be min 2').max(8, 'should be less than 8').required('length is required')
+})
+
 export default function App() {
+
   return (
     <View>
       <Text>App lore
